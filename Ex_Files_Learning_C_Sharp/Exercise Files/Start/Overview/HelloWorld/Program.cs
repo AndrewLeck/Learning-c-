aaabee49ff69d1bd36610244b8b3aca1e4ae6ed6
Andrew Leck <Aleck360@gmail.com>
2022-12-04 22:04:40 -0500

    Hello Andrew

--- Ex_Files_Learning_C_Sharp/Exercise Files/Start/Overview/HelloWorld/Program.cs
+++ Ex_Files_Learning_C_Sharp/Exercise Files/Start/Overview/HelloWorld/Program.cs
@@ -1,15 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main (string []args)
-        {
-            Console.WriteLine("Hello, World!");         
-        }
-    }
-}
+Console.WriteLine("Hello, World!");  
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();  
+Console.WriteLine("why, hello there" + str);      
 
 
