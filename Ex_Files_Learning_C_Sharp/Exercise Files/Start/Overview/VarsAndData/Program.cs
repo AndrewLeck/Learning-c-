3547dffcb78a423120320409c499c05b9fa7463b
Andrew Leck <Aleck360@gmail.com>
2022-12-06 15:54:23 -0500

    Variavles

--- Ex_Files_Learning_C_Sharp/Exercise Files/Start/Overview/VarsAndData/Program.cs
+++ Ex_Files_Learning_C_Sharp/Exercise Files/Start/Overview/VarsAndData/Program.cs
@@ -14,26 +14,30 @@ namespace VarsAndData
             char c = 'c';
 
             // Declare a string - it's a collection of characters
-            string str = "a string";
+            string str = "a string";  
 
             // Declare an implicit variable
             var x = 10;
             var z = "Hello!";
 
             // TODO: Declare an array of values
+            int[] vals = new int[5];
+            string[] strs = { "one", "two", "three"}
 
 
             // TODO: Print the values using a Formatting String
-
+            Console.WriteLine("{0},{1}, 2, 3, 4, 5, 6, 7", i, c, b, str, f, )
 
             // TODO: "null" means "no value"
-
-
+            object obj = null;
+            Console.WriteLine(obj)
             // TODO: Implicit conversion between types
-
+            long bignunb;
+            bignunb = i;
 
             // TODO: Explicit conversions
-
+            float i_to_f = (float)i;
+            Console.WriteLine
         }
     }
 }
