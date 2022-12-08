a661318f2c8c01f55c6fcc4f54a34970cf86b809
Andrew Leck <Aleck360@gmail.com>
2022-12-07 19:21:31 -0500

    defining classes

--- Ex_Files_Learning_C_Sharp/Exercise Files/Start/Classes/Defining/Program.cs
+++ Ex_Files_Learning_C_Sharp/Exercise Files/Start/Classes/Defining/Program.cs
@@ -7,14 +7,16 @@ namespace Defining
         static void Main(string[] args)
         {
             // TODO: Create new object instances using the "new" operator
-
+            Book b1 = new Book("war and Peace", "Leo Tolstoy", 850);
+            Book b2 = new Book("The graps of Wrath", "Leo Tolstoy", 509);
             
             // TODO: Call a method on the object
-
+            Console.WriteLine(b1.GetDescription());
+             Console.WriteLine(b2.GetDescription());
 
             // TODO: try to set one of the properties -- 
             // this will result in an error
-
+            b1._name = "Alux Huxly";
         }
     }
 }
